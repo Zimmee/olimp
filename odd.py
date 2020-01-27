d7697ef5db6dd5b2ffb3fa3d1bb77abc25758ffb
Zimmee <SHKAFERAK.M@GMAIL.COM>
2020-01-27 20:15:47 +0200

    master

--- odd.py
+++ odd.py
@@ -1,5 +1,18 @@
-import sys
-sys.stdin = open('aplusb.in','r')
-sys.stdout = open('aplusb.out','w')
+import sys, math
+sys.stdin = open('even-or-odd.in','r')
+sys.stdout = open('even-or-odd.out','w')
 n = int(input())
 a = map(int, input().split())
+def divide(n):
+    for i in range(1, int(n / 2) + 1):
+        if n % i == 0:
+            yield i
+    yield n
+    
+s=''  
+for j in a:
+    if j==1:
+        s+='1'
+    else:
+        s+=divide(j)
+print(s)
