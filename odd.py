1b0608237a1071002fda79e82ddcb4a22f9b082d
Zimmee <SHKAFERAK.M@GMAIL.COM>
2020-01-27 19:00:51 +0200

    master

--- odd.py
+++ odd.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin = open(’aplusb.in’,’r’)
-sys.stdout = open(’aplusb.out’,’w’)
+sys.stdin = open('aplusb.in','r')
+sys.stdout = open('aplusb.out','w')
 n = int(input())
 a = map(int, input().split())
